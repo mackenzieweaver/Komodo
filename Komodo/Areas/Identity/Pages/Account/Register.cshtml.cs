2a4f89f902a18664fe7e0bd0d5df480640d674bf
Mack <macknz7@gmail.com>
2020-11-12 13:24:08 -0800

    Register Implements FirstName / LastName

--- Komodo/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Komodo/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -48,6 +48,14 @@ namespace Komodo.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -76,7 +84,7 @@ namespace Komodo.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new BTUser { UserName = Input.Email, Email = Input.Email };
+                var user = new BTUser { FirstName = Input.FirstName, LastName = Input.LastName, UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
