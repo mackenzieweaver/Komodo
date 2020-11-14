37928fd42be8c69a0b5ae36055856efb954daf74
Mack <macknz7@gmail.com>
2020-11-13 17:53:25 -0800

    suggested actions box rough draft

--- Komodo/Controllers/HomeController.cs
+++ Komodo/Controllers/HomeController.cs
@@ -7,21 +7,80 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Komodo.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Komodo.Models.ViewModels;
+using Komodo.Data;
+using Microsoft.EntityFrameworkCore;
+using Komodo.Services;
 
 namespace Komodo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<BTUser> _userManager;
+        private readonly IBTProjectService _projectService;
+        private readonly IBTRolesService _rolesService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context, ILogger<HomeController> logger, UserManager<BTUser> userManager, IBTProjectService projectService, IBTRolesService rolesService)
         {
+            _context = context;
             _logger = logger;
+            _userManager = userManager;
+            _projectService = projectService;
+            _rolesService = rolesService;
         }
         [Authorize]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new HomePMViewModel();
+            if(await _userManager.IsInRoleAsync(user, "ProjectManager"))
+            {
+                var num = 5;
+                var tCount = _context.Tickets.ToList().Count;
+                num = tCount < num ? tCount : num;
+                for(var i = 0; i < num; i++)
+                {
+                    var ticket = _context.Tickets
+                        .Include(t => t.TicketPriority)
+                        .Include(t => t.TicketStatus)
+                        .Include(t => t.TicketType)
+                        .OrderBy(t => t.TicketPriorityId)
+                        .ThenBy(t => t.TicketStatusId)
+                        .Skip(i)
+                        .Take(1)
+                        .ToList()[0];
+                    // no dev on ticket
+                    if(ticket.DeveloperUserId == null)
+                    {
+                        // add ticket
+                        vm.Tickets.Add(ticket);
+
+                        var projects = await _projectService.ListUserProjects(user.Id);
+                        var users = new List<ICollection<BTUser>>();
+                        foreach (var project in projects)
+                        {
+                            users.Add(await _projectService.UsersOnProject(project.Id));
+                        }
+                        List<BTUser> flatUsers = users.SelectMany(u => u).Distinct().OrderBy(u => u.ProjectUsers.Count).ToList();
+                        List<BTUser> devs = new List<BTUser>();
+                        foreach (var flatuser in flatUsers)
+                        {
+                            if (await _rolesService.IsUserInRole(flatuser, "Developer"))
+                            {
+                                devs.Add(flatuser);
+                            }
+                        }
+                        vm.Developers.Add(devs[0]);
+
+                        var tickets = _context.Tickets.Where(t => t.DeveloperUserId == devs[0].Id).ToList();
+                        vm.Count.Add(tickets.Count);
+                    }
+                }
+            }
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ Komodo/Models/ViewModels/HomePMViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Komodo.Models.ViewModels
+{
+    public class HomePMViewModel
+    {
+        public HomePMViewModel()
+        {
+            Tickets = new List<Ticket>();
+            Developers = new List<BTUser>();
+            Count = new List<int>();
+        }
+        public List<Ticket> Tickets { get; set; }
+        public List<BTUser> Developers { get; set; }
+        public List<int> Count { get; set; }
+    }
+}
