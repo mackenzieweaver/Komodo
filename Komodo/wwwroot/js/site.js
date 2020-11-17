0dcd8c4b0c834cd2885e1b2f1268538d74eb8ec5
Mack <macknz7@gmail.com>
2020-11-17 12:36:57 -0800

    Donut chart moved to site.js

--- Komodo/wwwroot/js/site.js
+++ Komodo/wwwroot/js/site.js
@@ -3,5 +3,40 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
-    $('.ticketsIndex').DataTable();
+    $('.ticketsIndex').DataTable();      
+    InitDonut();
 });
+
+function InitDonut() {
+    var donutChart = {
+        chart: {
+            height: 350,
+            type: 'pie',
+            toolbar: {
+                show: false,
+            }
+        },
+
+        series: [10, 20, 30, 40],
+        labels: ["Admin", "Project Manager", "Developer", "Submitter"],
+
+        responsive: [{
+            breakpoint: 480,
+            options: {
+                chart: {
+                    width: 200
+                },
+                legend: {
+                    position: 'bottom'
+                }
+            }
+        }]
+    }
+
+    var donut = new ApexCharts(
+        document.querySelector("#donut-chart"),
+        donutChart
+    );
+
+    donut.render();
+}
